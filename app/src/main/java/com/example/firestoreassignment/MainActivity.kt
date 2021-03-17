cd8d309c7a8caad7605edd1d6ef65efabf6e8185
ahmed-madhoun1 <ahmed2madhoun2@gmail.com>
2021-03-17 11:53:15 +0300

    first commit

--- app/src/main/java/com/example/firestoreassignment/MainActivity.kt
+++ app/src/main/java/com/example/firestoreassignment/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.firestoreassignment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val firestore: FirebaseFirestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firestore.collection("Users")
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                querySnapshot?.forEach {
+                    textViewData.append(it.get("name").toString() + "|" +it.get("number").toString() + "|" + it.get("address").toString() + "\n")
+
+                }
+            }
+
+        btnAddUser.setOnClickListener {
+            addDataToFirestore(
+                user = User(
+                    editTextName.text.toString(),
+                    editNumber.text.toString(),
+                    editAddress.text.toString()
+                )
+            )
+        }
+    }
+
+    private fun addDataToFirestore(user: User) {
+        firestore.collection("Users").add(user).addOnSuccessListener {
+            Toast.makeText(this, "User Added", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 }
